94da784a76632bf4ab3936635918ed57caca2673
akeegan97 <andrewkeegan97@gmail.com>
2022-05-30 18:39:56 -0400

    SARIMA time series forecasting model built

--- Super_Market_Example_Project_V2.py
+++ Super_Market_Example_Project_V2.py
@@ -3,7 +3,10 @@
 # file path of data : "C:\Users\Andrew\Desktop\Python_Projects\Super_Market_Project\Sample - Superstore.csv"
 #Dataset given by Kaggle.com open datasets
 
+from pickle import TRUE
 from statistics import mode
+from turtle import color
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
@@ -205,29 +208,216 @@ def group(dataframe):
     return df3
 
 sales_furniture = group(furniture_df)
-""" print(sales_furniture) """
+""" print(sales_furniture)  """
 
 y= sales_furniture['Sales']
 y = y.droplevel(level=0)
 y1 = sales_furniture['Profit']
+y1 = y1.droplevel(level=0)
+y2 = sales_furniture['Quantity']
+y2 = y2.droplevel(level=0)
+
 
 import statsmodels.api as sm
 y.index=y.index.to_timestamp()
+y1.index=y1.index.to_timestamp()
+y2.index=y2.index.to_timestamp()
+
 def seasonal_decompose(y):
     decomposition = sm.tsa.seasonal_decompose(y,model='additive',)
     fig =decomposition.plot()
     fig.set_size_inches(14,7)
     plt.show()
 
-seasonal_decompose(y)
+""" seasonal_decompose(y) """
 
 """ print(y) """
 
 
+#Building forecasting model based on the SARIMA
+#####SALES#####
+##testing Stationarity
+
+def test_stationarity(timeseries, title):
+    rolmean = pd.Series(timeseries).rolling(window=12).mean()
+    rolstd = pd.Series(timeseries).rolling(window=12).std()
+
+
+    fig, ax = plt.subplots(figsize = (16,4))
+    ax.plot(timeseries,label = title)
+    ax.plot(rolmean, label = 'Rolling Mean')
+    ax.plot(rolstd,label = 'Rolling STD (x10)')
+    ax.legend()
+    plt.show()
+    
+
+pd.options.display.float_format = '{:.8f}'.format
+
+""" test_stationarity(y,'Raw Data') """
+
+
+#second test of stationarity Augmented Dickery-Fuller Test
+from statsmodels.tsa.stattools import adfuller
+
+def ADF_test(timeseries, dataDesc):
+    print('> Is the {} stationary ?'.format(dataDesc))
+    dftest = adfuller(timeseries.dropna(), autolag='AIC')
+    print('Test statistic = {:.3f}'.format(dftest[0]))
+    print('P-value = {:.3f}'.format(dftest[1]))
+    print('Critical values :')
+    for k, v in dftest[4].items():
+        print('\t{}: {} - the data is {} stationary with {}% confidence'.format(k,v,'not' if v < dftest[0] else '', 100-int(k[:-1])))
+
+""" ADF_test(y,'RawData') """
+
+""" print(y) """
+
+y_to_train = y[:'2016-12-01']
+y_to_val = y[:'2017-12-01']
+predict_date = len(y)-len(y_to_val)
+
+
+import itertools
+
+def sarima_grid_search(y, seasonal_period):
+    p = d = q  =range(0,2)
+    pdq = list(itertools.product(p,d,q))
+    seasonal_pdq = [(x[0],x[1],x[2], seasonal_period) for x in list(itertools.product(p,d,q))]
+
+    mini = float('+inf')
+
+
+    for param in pdq:
+        for param_seasonal in seasonal_pdq:
+            try: 
+                mod = sm.tsa.statespace.SARIMAX(y,
+                order=param,
+                seasonal_order=param_seasonal,
+                enforce_stationarity=False,
+                enforce_invertibility=False)
+                results = mod.fit()
+                if results.aic <mini:
+                    mini = results.aic
+                    param_mini = param
+                    param_seasonal_mini = param_seasonal
+#print('SARIMA{}x{} - AIC:{}'.format(param,param_seasonal,results.aic))
+            except:
+                continue
+    print('the set of params with min AIC is SARIMA{}x{} - AIC:{}'.format(param_mini, param_seasonal_mini, mini))
+
+""" sarima_grid_search(y,12) """
+
+
+def sarima_eva(y, order, seasonal_order, seasonal_period, pred_date, y_to_test):
+    mod = sm.tsa.statespace.SARIMAX(
+        y, 
+    order=order,
+    seasonal_order=seasonal_order,
+    enforce_invertibility=False,
+    )
+    results = mod.fit()
+    print(results.summary().tables[1])
+
+    results.plot_diagnostics(figsize=(16,8))
+    plt.show()
 
 
 
+    pred = results.get_prediction(start=pd.to_datetime(pred_date),dynamic = False)
+    pred_ci = pred.conf_int()
+    y_forecasted = pred.predicted_mean
+    mse = ((y_forecasted - y_to_test)**2).mean()
+    print('The Root Mean Squared Error of Sarima w/ seasonal_length={} and dynamic = False {}'.format(seasonal_period,round(np.sqrt(mse),2)))
 
+    ax = y.plot(label = 'Observed')
+    y_forecasted.plot(ax=ax, label = 'One-Step ahead Forecast', alpha = .7, figsize = (14,7))
+    ax.fill_between(pred_ci.index,
+    pred_ci.iloc[:,0],
+    pred_ci.iloc[:,1], color = 'k', alpha = .2)
+
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Sessions')
+    plt.legend()
+    plt.show()
+
+
+    pred_dynamic = results.get_prediction(start = pd.to_datetime(pred_date), dynamic = True, full_results = True)
+    pred_dynamic_ci = pred_dynamic.conf_int()
+    y_forecasted_dynamic = pred_dynamic.predicted_mean
+    mse_dynamic = ((y_forecasted_dynamic - y_to_test)**2).mean()
+    print('The Root Mean Square Error of Sarima w/season_length = {} and dynamic = TRUE {}'.format(seasonal_period,round(np.sqrt(mse_dynamic),2)))
+    ax = y.plot(label = 'Observed')
+    y_forecasted_dynamic.plot(label='Dynamic Forecast', ax=ax, figsize = (14,7))
+    ax.fill_between(pred_dynamic_ci.index,
+    pred_dynamic_ci.iloc[:,0],
+    pred_dynamic_ci.iloc[:,1], color = 'k', alpha = .2)
+
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Sessions')
+    plt.legend()
+    plt.show()
+
+    return(results)
+
+
+""" model = sarima_eva(y,(0,1,1),(0,1,1,12),12,'2016-12-01',y_to_val) """
+
+
+
+##predictive results for sales:
+
+def forecast(model, predict_steps, y):
+    pred_uc = model.get_forecast(steps = predict_steps)
+
+    pred_ci = pred_uc.conf_int()
+    
+
+    ax = y.plot(label = 'Observed', figsize= (14,7))
+    pred_uc.predicted_mean.plot(ax=ax, label = "Forecast")
+    ax.fill_between(pred_ci.index,
+    pred_ci.iloc[:,0],
+    pred_ci.iloc[:,1], color = 'k', alpha = .25)
+    ax.set_xlabel('Date')
+    ax.set_ylabel(y.name)
+
+    plt.legend()
+    plt.show()
+
+
+    pm = pred_uc.predicted_mean.reset_index()
+    pm.columns = ['Date', 'Predicted_Mean']
+    pci = pred_ci.reset_index()
+    pci.columns = ['Date', 'Lower Bound', 'Upper Bound']
+    final_table = pm.join(pci.set_index('Date'), on = 'Date')
+
+    return(final_table)
+
+""" final_table = forecast(model,12,y) """
+""" print(final_table) """
+
+""" print(sales_furniture) """
+
+###PROFIT###
+#1Seasonality Decomposition
+#seasonal_decompose(y1)
+#2 Stationarity
+#test_stationarity(y1,'Profit')
+#3 ADF test
+#ADF_test(y1,'Profit')
+#4Grid search for SARIMA paramaters:
+#sarima_grid_search(y1,12)
+#5 Model for SARIMA w/PARAMS^
+#model_1 = sarima_eva(y1,(0,1,1),(0,1,1,12),12,'2016-12-01',y1_to_val)
+#forecast(model,12,y)
+y1_to_eval = y1[:'2016-12-01']
+y1_to_val = y1[:'2017-12-01']
+predict_date = len(y1)-len(y1_to_val)
+
+""" sarima_eva(y1,(0,1,1),(0,1,1,12),12,'2016-12-01',y1_to_eval)
+
+model_1 = sarima_eva(y1,(0,1,1),(0,1,1,12),12,'2016-12-01',y1_to_val)
+
+forecast(model_1,12,y1)  """
 
 
 
